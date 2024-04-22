76a66764dbccb85cb02ddce86b43149447262fc8
TM-Sabarish <sabarishtm22@gmail.com>
2024-04-22 14:26:06 +0530

    Update SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,18 +1,20 @@
 import * as React from "react";
-import { styled, useTheme } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import MuiDrawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
 import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
 import QueueMusicIcon from '@mui/icons-material/QueueMusic';
 import HomeIcon from '@mui/icons-material/Home';
+import LogoutIcon from '@mui/icons-material/Logout'; // Import logout icon
 import { useNavigate } from 'react-router-dom';
 
-const drawerWidth = 72; // Adjusted width for mini sidebar
+const drawerWidth = 110;
+const iconSize = 30;
 
 const Drawer = styled(MuiDrawer, {
   shouldForwardProp: (prop) => prop !== "open",
@@ -27,37 +29,61 @@ const Drawer = styled(MuiDrawer, {
   },
 }));
 
+const StyledListItemButton = styled(ListItemButton)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+}));
+
+const StyledListItemText = styled(Typography)(({ theme }) => ({
+  textAlign: "center",
+}));
+
 export default function SideBar() {
   const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate('/login')
+  };
 
   return (
     <Drawer variant="permanent">
       <Divider />
       <List>
         <ListItem disablePadding onClick={() => navigate('/')}>
-          <ListItemButton>
+          <StyledListItemButton>
             <ListItemIcon>
-              <HomeIcon />
+              <HomeIcon sx={{ fontSize: 35, mb: 2, ml: 1 }} />
             </ListItemIcon>
-            <ListItemText primary="Home" sx={{ textAlign: 'center' }} />
-          </ListItemButton>
+          </StyledListItemButton>
         </ListItem>
         <Divider />
         <ListItem disablePadding onClick={() => navigate('/dashboard')}>
-          <ListItemButton>
+          <StyledListItemButton>
             <ListItemIcon>
-              <DashboardOutlinedIcon />
+              <DashboardOutlinedIcon sx={{ fontSize: iconSize, mb: 2, ml: 1, mt: 1 }} />
             </ListItemIcon>
-            <ListItemText primary="Dashboard" sx={{ textAlign: 'center' }} />
-          </ListItemButton>
+            <StyledListItemText sx={{ fontSize: 15 }}>Dashboard</StyledListItemText>
+          </StyledListItemButton>
         </ListItem>
         <ListItem disablePadding onClick={() => navigate('/course')}>
-          <ListItemButton>
+          <StyledListItemButton>
             <ListItemIcon>
-              <QueueMusicIcon />
+              <QueueMusicIcon sx={{ fontSize: iconSize, mb: 2, ml: 1, mt: 1 }} />
             </ListItemIcon>
-            <ListItemText primary="Music" sx={{ textAlign: 'center' }} />
-          </ListItemButton>
+            <StyledListItemText sx={{ fontSize: 15 }}>Course</StyledListItemText>
+          </StyledListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
+      <List sx={{ marginTop: 'auto' }}>
+        <ListItem disablePadding onClick={handleLogout}>
+          <StyledListItemButton>
+            <ListItemIcon>
+              <LogoutIcon sx={{ fontSize: iconSize, mb: 2, ml: 1 }} />
+            </ListItemIcon>
+            <StyledListItemText sx={{ fontSize: 15 }}>Logout</StyledListItemText>
+          </StyledListItemButton>
         </ListItem>
       </List>
     </Drawer>
